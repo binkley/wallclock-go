bb3bbbe31eb640cc31b8875024f7521288def1cc
Brian Oxley <boxley@thoughtworks.com>
2017-01-06 15:59:32 -0600

    First passing test

--- wallclock.go
+++ wallclock.go
@@ -1,1 +1,14 @@
 package wallclock
+
+import (
+    "fmt"
+    "strings"
+)
+
+func Minutes(minute int) ([]string, error) {
+    switch(minute) {
+    case 5:
+        return strings.Fields("five past"), nil
+    }
+    return nil, fmt.Errorf("wallclock.Minutes: out of range: %d", minute)
+}
--- wallclock_test.go
+++ wallclock_test.go
@@ -3,4 +3,31 @@ package wallclock
 import "testing"
 
 func TestMinutes(t *testing.T) {
+    cases := []struct {
+        minute int
+        want []string
+    }{
+        {5, []string{"five", "past"}},
+    }
+    for _, c := range cases {
+        got, err := Minutes(c.minute)
+        if err != nil {
+            t.Error(err)
+        }
+        if !StringArrayEquals(c.want, got) {
+            t.Errorf("Minutes(%d) == %q, want %q", c.minute, got, c.want)
+        }
+    }
+}
+
+func StringArrayEquals(a []string, b []string) bool {
+    if len(a) != len(b) {
+        return false
+    }
+    for i, v := range a {
+        if v != b[i] {
+            return false
+        }
+    }
+    return true
 }
